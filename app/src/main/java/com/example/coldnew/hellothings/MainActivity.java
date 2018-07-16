896bab241d3bd3b324dfc40730c593356645fb5a
sean <present810209@gmail.com>
2018-07-16 13:44:09 +0800

    aaa

--- app/src/main/java/com/example/coldnew/hellothings/MainActivity.java
+++ app/src/main/java/com/example/coldnew/hellothings/MainActivity.java
@@ -5,13 +5,16 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
 import com.google.android.things.pio.Gpio;
-
 import com.google.android.things.pio.PeripheralManager;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "HelloThings";
@@ -21,14 +24,20 @@ public class MainActivity extends AppCompatActivity {
     private Handler mHandler = new Handler();
     private Gpio mLedGpio;
     PeripheralManager service;
+    DatabaseReference myRef;
+    FirebaseDatabase database;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        ActivityCompat.requestPermissions(this, new String[]{
+        ActivityCompat.requestPermissions(this, new String[]{"com.google.android.things.permission.MANAGE_INPUT_DRIVERS",
                 "com.google.android.things.permission.USE_PERIPHERAL_IO"}, 0);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
 
     }
 
@@ -47,33 +56,48 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
-    protected void onStart(){
-        super.onStart();
 
-        try {
-            service = PeripheralManager.getInstance();
-            mLedGpio =service.openGpio(LED);
-            mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
-            mLedGpio.setValue(true);
-            Log.i(TAG, "Start blinking LED by GPIO21");
-            mHandler.post(mBlinkRunnable);
-        } catch (IOException e) {
-            Log.e(TAG, "Error on PeripheralIO API", e);
-        }
+    public void onResume() {
+        super.onResume();
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("aaaa", "Value is: " + value);
+                if(value.equals("true")){
+                    try {
+                        service = PeripheralManager.getInstance();
+                        mLedGpio =service.openGpio(LED);
+                        mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+                        mLedGpio.setValue(true);
+                        Log.i(TAG, "Start blinking LED by GPIO21");
+                        mHandler.post(mBlinkRunnable);
+                    } catch (IOException e) {
+                        Log.e(TAG, "Error on PeripheralIO API", e);
+                    }
+                }
+                else {
+                    mHandler.removeCallbacks(mBlinkRunnable); // <---- Add this
+                    Log.i(TAG, "Closing LED GPIO21 pin");
+                    try {
+                        mLedGpio.close();
+                    } catch (IOException e) {
+                        Log.e(TAG, "Error on PeripheralIO API", e);
+                    } finally {
+                        mLedGpio = null;
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.d("aaaa", "Value is: " + "error");
+            }
+        });
     }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mHandler.removeCallbacks(mBlinkRunnable); // <---- Add this
-        Log.i(TAG, "Closing LED GPIO21 pin");
-        try {
-            mLedGpio.close();
-        } catch (IOException e) {
-            Log.e(TAG, "Error on PeripheralIO API", e);
-        } finally {
-            mLedGpio = null;
-        }
-    }
 
+    }
 }
